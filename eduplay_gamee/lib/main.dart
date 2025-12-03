661106076dfae44284cf22cbd059d13b343bb8f6
ZyadMewing <rifqyziyadfulvian@gmail.com>
2025-12-03 22:19:38 +0700

    done

---
+++ eduplay_gamee/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'screens/registration_screen.dart';
+import 'screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'services/audio_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Wajib ada jika main() async
+  await AudioManager.instance.init(); // Siapkan Audio
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Cek User Logic (Simpel)
+  Future<bool> _isUserRegistered() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userName') != null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:
+          false, // <--- INI YANG MENGHILANGKAN TULISAN DEBUG
+      title: 'EduPlay',
+      theme: ThemeData(
+        fontFamily: 'Poppins', // Jika punya font custom, atau hapus baris ini
+        primarySwatch: Colors.blue,
+        useMaterial3: true, // Gunakan Material 3 agar UI lebih modern
+      ),
+      home: FutureBuilder<bool>(
+        future: _isUserRegistered(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          if (snapshot.hasData && snapshot.data == true) {
+            return const HomeScreen();
+          } else {
+            return const RegistrationScreen();
+          }
+        },
+      ),
+    );
+  }
+}
